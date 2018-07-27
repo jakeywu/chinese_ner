a076f18e750c34ae18637c6c97becfb865d8d1ad
吴万杰 <wanjie.wu@socialcredits.cn>
2018-07-27 16:33:22 +0800

    alter prepare data logic

--- model/data_utils.py
+++ model/data_utils.py
@@ -1,43 +1,80 @@
-import json
-import codecs
 import os
+import copy
+import codecs
 
 
 class PrepareTagData(object):
-    def __init__(self):
-        prj = os.path.abspath(os.path.join(__file__, os.path.pardir, os.path.pardir))
-        self.trainPath = os.path.join(prj, "data/trainset")
-        self.chineseVocab = os.path.join(prj, "data/vocab_dict.txt")
-        self.dataSet = os.path.join(prj, "data/dataset.txt")
-        self.vocabDict = self._load_chinese_vocab()
+    def __init__(self, conf, mode="train"):
+        """
+        mode: is what dataset can ben used. can be [train/test/validate]
+        :param conf:
+        :param mode:
+        """
+        self.config = conf
+        self.mode = mode
+        self.vocabDict = self.__load_chinese_vocab()
         self.tagId = self._tag_id()
-
-    def _load_chinese_vocab(self):
-        with codecs.open(self.chineseVocab, "r", "utf8") as f:
-            return json.loads(f.read())
+        self._sourceData = self.__read_dataset()
 
     @staticmethod
-    def _tag_id():
-        """
-        O other
-        B begin
-        I
-        E end
-        :return:
-        """
-        return {
-            "O": 0,
-            "B-S-ORG": 1,
-            "I-S-ORG": 2,
-            "E-S-ORG": 3,
-        }
+    def __load_chinese_vocab():
+        cv = dict()
+        with codecs.open(os.path.join(os.path.dirname(os.getcwd()), "data/chinese_vocab.txt"), "r", "utf8") as f:
+            for i, line in enumerate(f.readlines()):
+                cv[line.strip()] = i
+        return cv
 
-    def train_data(self):
-        sentence_lst = self._load_train_tag()
-        return self.__split_train_data(sentence_lst)
+    def __read_dataset(self):
+        if self.mode == "train":
+            dataset_path = os.path.join(os.path.dirname(os.getcwd()), "data/trainset.txt")
+        elif self.mode == "test":
+            dataset_path = os.path.join(os.path.dirname(os.getcwd()), "data/testset.txt")
+        else:
+            raise Exception("mode must be in [train/test]")
+        if not os.path.exists(dataset_path):
+            raise Exception("path [{}] not exists".format(dataset_path))
+        with codecs.open(dataset_path, "r", "utf8") as fp:
+            while True:
+                a_line = fp.readline()
+                if a_line:
+                    yield a_line.strip()
+                else:
+                    break
+
+    def _tag_id(self):
+        tag_dict = dict()
+        for i, v in enumerate(self.config.tag_char.split(",")):
+            tag_dict[v] = i
+        return tag_dict
+
+    def __is_end_sentence(self, cur):
+        if cur.endswith(self.config.dataset_flag):
+            return True
+        return False
+
+    def __next__(self):
+        sentence_lst = []
+        count = 0
+        try:
+            sentence = []
+            while count < self.config.batch_size:
+                cur = next(self._sourceData)
+                if self.__is_end_sentence(cur):
+                    count += 1
+                    sentence_lst.append(copy.deepcopy(sentence))
+                    sentence = []
+                else:
+                    sentence.append(cur)
+
+        except StopIteration as iter_exception:
+            if count == 0:
+                raise iter_exception
+        deal_sentence_lst = self.__deal_batch_data(sentence_lst)
+        input_x, input_y = self.__split_batch_data(deal_sentence_lst)
+        return input_x, input_y
 
     @staticmethod
-    def __split_train_data(sentence_lst):
+    def __split_batch_data(sentence_lst):
         inputs_x = []
         inputs_y = []
         for sentence in sentence_lst:
@@ -45,25 +82,36 @@ class PrepareTagData(object):
             inputs_y.append([item[1] for item in sentence])
         return inputs_x, inputs_y
 
-    def _load_train_tag(self):
-        with codecs.open(self.trainPath, "r", "utf8") as f:
-            sentence_lst = []
-            char_lst = []
-            for line in f.readlines():
-                line = line.replace("\n", "").replace("B-ORG", "O").replace("I-ORG", "")
-                if line != "end":
-                    line = line.split(" ")
-                    vocab_id = self.vocabDict.get(line[0], -1)
-                    if vocab_id == -1:
-                        continue
-                    tag_id = self.tagId.get(line[1], -1)
-                    if tag_id == -1:
-                        continue
-                    char_lst.append([vocab_id, tag_id])
-                else:
-                    sentence_lst.append(char_lst)
-                    char_lst = []
+    def __deal_batch_data(self, sentence_lst):
+        sentence_ids = []
+        for sentence in sentence_lst:
+            char_ids = []
+            for line in sentence:
+                line = line.split(" ")
+                vocab_id = self.vocabDict.get(line[0], -1)
+                if vocab_id == -1:
+                    continue
+                tag_id = self.tagId.get(line[1], -1)
+                if tag_id == -1:
+                    continue
+                char_ids.append([vocab_id, tag_id])
+            sentence_ids.append(char_ids)
+        return sentence_ids
 
-        # with codecs.open(self.dataSet, "w", "utf8") as f:
-        #     f.write(json.dumps(sentence_lst, indent=2, ensure_ascii=False))
-        return sentence_lst
+
+if __name__ == "__main__":
+    class CFG:
+        batch_size = 10
+        tag_char = "O,B-S-ORG,I-S-ORG,E-S-ORG"
+        dataset_flag = "end"
+
+
+    cfg = CFG()
+    a = PrepareTagData(cfg, "train")
+    while True:
+        try:
+            train_x, train_y = next(a)
+            print(train_x)
+            print(train_y)
+        except Exception as e:
+            exit()
--- train_model.py
+++ train_model.py
@@ -4,17 +4,21 @@ from model.rnn_cnn_crf import RnnCnnCrf
 
 tf.flags.DEFINE_integer(name="num_tag", default=4, help="number tags")
 tf.flags.DEFINE_integer(name="epoch", default=10, help="maximum epochs")
+tf.flags.DEFINE_integer(name="batch_size", default=10, help="batch size")
 tf.flags.DEFINE_integer(name="vocab_size", default=5000, help="vocab num")
 tf.flags.DEFINE_integer(name="num_hidden", default=128, help="lstm num hidden")
 tf.flags.DEFINE_integer(name="embedding_size", default=128, help="embedding size")
 tf.flags.DEFINE_integer(name="filter_size", default=3, help="cnn filter size")
 tf.flags.DEFINE_integer(name="filter_num", default=128, help="cnn filter num")
+
+tf.flags.DEFINE_float(name="train_ratio", default=0.7, help="only include train/test, no validate")
+tf.flags.DEFINE_string(name="dataset_flag", default="end", help="split dataset sentence by end")
+tf.flags.DEFINE_string(name="tag_char", default="O,B-S-ORG,I-S-ORG,E-S-ORG", help="used in dataset, split by ,")
+
 FLAG = tf.flags.FLAGS
 
 
 if __name__ == "__main__":
-    train_x, train_y = PrepareTagData().train_data()
+    train_x, train_y = PrepareTagData(FLAG)
     model = RnnCnnCrf(FLAG)
-    import pdb
-    pdb.set_trace()
     tf.app.run()
