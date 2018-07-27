e1e419be5b662127cd08131c42bcbe4ceac00043
吴万杰 <wanjie.wu@socialcredits.cn>
2018-07-27 11:27:33 +0800

    fix bug

--- model/rnn_cnn_crf.py
+++ model/rnn_cnn_crf.py
@@ -20,18 +20,37 @@ class BaseModel(object):
         saver = tf.train.Saver()
         saver.restore(self.sess, tf.train.latest_checkpoint(BaseModel.checkpointPath))
 
+    @staticmethod
+    def _cnn_2d(inputs, scope_name, filter_height, filter_width, in_channels, out_channel):
+        with tf.variable_scope(name_or_scope=scope_name):
+            filters = tf.get_variable(
+                name="W", shape=[filter_height, filter_width, in_channels, out_channel], dtype=tf.float32,
+                initializer=tf.truncated_normal_initializer(stddev=0.01))
+            bias = tf.get_variable(name="b", shape=[out_channel], dtype=tf.float32, initializer=tf.constant_initializer())
+            con2d_op = tf.nn.conv2d(input=inputs, filter=filters, strides=[1, 1, 1, 1], padding="SAME")
+        return tf.nn.bias_add(value=con2d_op, bias=bias)
 
-class RnnCnnCrf(object):
+    @staticmethod
+    def _cnn_max_pool(inputs, scope_name, ksize):
+        with tf.variable_scope(name_or_scope=scope_name):
+            return tf.nn.max_pool(value=inputs, ksize=[1, ksize, 1, 1], strides=[1, 1, 1, 1], padding="SAME")
+
+
+class RnnCnnCrf(BaseModel):
     def __init__(self, conf):
+        super(RnnCnnCrf, self).__init__()
         self.epoch = conf.epoch
         self.embedding_size = conf.embedding_size
         self.vocab_size = conf.vocab_size
         self.num_hidden = conf.num_hidden
         self.num_tag = conf.num_tag
+        self.filter_size = conf.filter_size
+        self.filter_num = conf.filter_num
 
         self._init_placeholder()
         self._embedding_layers()
         self._bi_lstm_layers()
+        self._cnn_layers()
 
     def _init_placeholder(self):
         self.inputs = tf.placeholder(dtype=tf.int32, shape=[None, None], name="inputs")
@@ -50,21 +69,25 @@ class RnnCnnCrf(object):
 
     def _bi_lstm_layers(self):
         with tf.variable_scope(name_or_scope="biLSTM_layers"):
-            shape = tf.shape(self.embedded_inputs)
             cell_fw = tf.nn.rnn_cell.LSTMCell(num_units=self.num_hidden)
             cell_bw = tf.nn.rnn_cell.LSTMCell(num_units=self.num_hidden)
             (output_fw, output_bw), _ = tf.nn.bidirectional_dynamic_rnn(
                 cell_fw, cell_bw, inputs=self.embedded_inputs, sequence_length=self.sequence_len,
                 time_major=False, dtype=tf.float32)
-            outputs = tf.nn.dropout(tf.concat([output_fw, output_bw], axis=2), keep_prob=self.keep_prob)
-            print(outputs)
-            # bi_outputs = tf.reshape(outputs, [-1, 2 * self.num_hidden])
-            # with tf.variable_scope(name_or_scope="proj"):
-            #     weight = tf.get_variable(name="W_out", shape=[2 * self.num_hidden, self.num_tag], dtype=tf.float32,
-            #                              initializer=tf.truncated_normal_initializer(stddev=0.1))
-            #     b = tf.get_variable(name="b", shape=[self.num_tag], dtype=tf.float32, initializer=tf.zeros_initializer())
-            #     pred = tf.matmul(bi_outputs, weight) + b
-            #     self.lstm_logits = tf.reshape(pred, [-1, shape[1], self.num_tag])
+            self.lstm_ouputs = tf.nn.dropout(tf.concat([output_fw, output_bw], axis=2), keep_prob=self.keep_prob)
 
     def _cnn_layers(self):
-        pass
+        with tf.variable_scope(name_or_scope="cnn_layers"):
+            self.embedded_inputs_expanded = tf.expand_dims(self.embedded_inputs, -1)
+            conv1 = self._cnn_2d(
+                inputs=self.embedded_inputs_expanded, scope_name="conv", filter_height=self.filter_size,
+                filter_width=self.embedding_size, in_channels=1, out_channel=self.filter_num
+            )
+            conv1 = tf.nn.relu(conv1)
+            import pdb
+            pdb.set_trace()
+            conv1 = self._cnn_max_pool(inputs=conv1, scope_name="max_pool", ksize=self.sequence_len-self.filter_size + 1)
+            print("="*10)
+            import pdb
+            pdb.set_trace()
+            print(conv1)
--- train_model.py
+++ train_model.py
@@ -7,6 +7,8 @@ tf.flags.DEFINE_integer(name="epoch", default=10, help="maximum epochs")
 tf.flags.DEFINE_integer(name="vocab_size", default=5000, help="vocab num")
 tf.flags.DEFINE_integer(name="num_hidden", default=128, help="lstm num hidden")
 tf.flags.DEFINE_integer(name="embedding_size", default=128, help="embedding size")
+tf.flags.DEFINE_integer(name="filter_size", default=3, help="cnn filter size")
+tf.flags.DEFINE_integer(name="filter_num", default=128, help="cnn filter num")
 FLAG = tf.flags.FLAGS
 
 
